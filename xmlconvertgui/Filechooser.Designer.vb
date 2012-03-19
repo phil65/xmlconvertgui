2c7327ee668fea2065830103548406d63d5cda00
phil65 <philipptemminghoff@googlemail.com>
2012-03-19 21:38:33 +0100

    Initial Commit

---
+++ xmlconvertgui/Filechooser.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Filechooser
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.ChooseButton = New System.Windows.Forms.Button()
+        Me.ConvertButton = New System.Windows.Forms.Button()
+        Me.xmlname = New System.Windows.Forms.Label()
+        Me.OutputButton = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OutputLabel = New System.Windows.Forms.Label()
+        Me.OutputLog = New System.Windows.Forms.TextBox()
+        Me.ConversionDropDown = New System.Windows.Forms.ComboBox()
+        Me.HeaderOption = New System.Windows.Forms.CheckBox()
+        Me.EncodingDropDown = New System.Windows.Forms.ComboBox()
+        Me.DebugOutput = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog.Multiselect = True
+        '
+        'ChooseButton
+        '
+        Me.ChooseButton.Location = New System.Drawing.Point(49, 22)
+        Me.ChooseButton.Name = "ChooseButton"
+        Me.ChooseButton.Size = New System.Drawing.Size(75, 23)
+        Me.ChooseButton.TabIndex = 1
+        Me.ChooseButton.Text = "Choose"
+        Me.ChooseButton.UseVisualStyleBackColor = True
+        '
+        'ConvertButton
+        '
+        Me.ConvertButton.Enabled = False
+        Me.ConvertButton.Location = New System.Drawing.Point(166, 214)
+        Me.ConvertButton.Name = "ConvertButton"
+        Me.ConvertButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConvertButton.TabIndex = 3
+        Me.ConvertButton.Text = "Convert"
+        Me.ConvertButton.UseVisualStyleBackColor = True
+        '
+        'xmlname
+        '
+        Me.xmlname.AutoSize = True
+        Me.xmlname.Location = New System.Drawing.Point(163, 27)
+        Me.xmlname.Name = "xmlname"
+        Me.xmlname.Size = New System.Drawing.Size(98, 13)
+        Me.xmlname.TabIndex = 4
+        Me.xmlname.Text = "Choose XML File(s)"
+        '
+        'OutputButton
+        '
+        Me.OutputButton.Location = New System.Drawing.Point(49, 84)
+        Me.OutputButton.Name = "OutputButton"
+        Me.OutputButton.Size = New System.Drawing.Size(75, 24)
+        Me.OutputButton.TabIndex = 5
+        Me.OutputButton.Text = "Choose"
+        Me.OutputButton.UseVisualStyleBackColor = True
+        Me.OutputButton.Visible = False
+        '
+        'OutputLabel
+        '
+        Me.OutputLabel.Location = New System.Drawing.Point(163, 90)
+        Me.OutputLabel.Name = "OutputLabel"
+        Me.OutputLabel.Size = New System.Drawing.Size(203, 61)
+        Me.OutputLabel.TabIndex = 6
+        Me.OutputLabel.Text = "Choose Output Folder"
+        Me.OutputLabel.Visible = False
+        '
+        'OutputLog
+        '
+        Me.OutputLog.Location = New System.Drawing.Point(351, 22)
+        Me.OutputLog.Multiline = True
+        Me.OutputLog.Name = "OutputLog"
+        Me.OutputLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.OutputLog.Size = New System.Drawing.Size(362, 284)
+        Me.OutputLog.TabIndex = 7
+        '
+        'ConversionDropDown
+        '
+        Me.ConversionDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ConversionDropDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ConversionDropDown.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.ConversionDropDown.Location = New System.Drawing.Point(12, 192)
+        Me.ConversionDropDown.Name = "ConversionDropDown"
+        Me.ConversionDropDown.Size = New System.Drawing.Size(121, 21)
+        Me.ConversionDropDown.TabIndex = 8
+        '
+        'HeaderOption
+        '
+        Me.HeaderOption.AutoSize = True
+        Me.HeaderOption.Location = New System.Drawing.Point(12, 275)
+        Me.HeaderOption.Name = "HeaderOption"
+        Me.HeaderOption.Size = New System.Drawing.Size(108, 17)
+        Me.HeaderOption.TabIndex = 9
+        Me.HeaderOption.Text = "Add XML Header"
+        Me.HeaderOption.UseVisualStyleBackColor = True
+        '
+        'EncodingDropDown
+        '
+        Me.EncodingDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.EncodingDropDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.EncodingDropDown.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.EncodingDropDown.Location = New System.Drawing.Point(12, 233)
+        Me.EncodingDropDown.Name = "EncodingDropDown"
+        Me.EncodingDropDown.Size = New System.Drawing.Size(121, 21)
+        Me.EncodingDropDown.TabIndex = 10
+        '
+        'DebugOutput
+        '
+        Me.DebugOutput.AutoSize = True
+        Me.DebugOutput.Location = New System.Drawing.Point(166, 275)
+        Me.DebugOutput.Name = "DebugOutput"
+        Me.DebugOutput.Size = New System.Drawing.Size(115, 17)
+        Me.DebugOutput.TabIndex = 11
+        Me.DebugOutput.Text = "Enable Debug Log"
+        Me.DebugOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.DebugOutput.UseVisualStyleBackColor = True
+        '
+        'Filechooser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 318)
+        Me.Controls.Add(Me.DebugOutput)
+        Me.Controls.Add(Me.EncodingDropDown)
+        Me.Controls.Add(Me.HeaderOption)
+        Me.Controls.Add(Me.ConversionDropDown)
+        Me.Controls.Add(Me.OutputLog)
+        Me.Controls.Add(Me.OutputLabel)
+        Me.Controls.Add(Me.OutputButton)
+        Me.Controls.Add(Me.xmlname)
+        Me.Controls.Add(Me.ConvertButton)
+        Me.Controls.Add(Me.ChooseButton)
+        Me.Name = "Filechooser"
+        Me.Text = "Skin XML Converter - by phil65"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ChooseButton As System.Windows.Forms.Button
+    Friend WithEvents ConvertButton As System.Windows.Forms.Button
+    Friend WithEvents xmlname As System.Windows.Forms.Label
+    Friend WithEvents OutputButton As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents OutputLabel As System.Windows.Forms.Label
+    Friend WithEvents OutputLog As System.Windows.Forms.TextBox
+    Friend WithEvents ConversionDropDown As System.Windows.Forms.ComboBox
+    Friend WithEvents HeaderOption As System.Windows.Forms.CheckBox
+    Friend WithEvents EncodingDropDown As System.Windows.Forms.ComboBox
+    Friend WithEvents DebugOutput As System.Windows.Forms.CheckBox
+
+End Class
---
+++ xmlconvertgui/Filechooser.vb
@@ -0,0 +1,401 @@
+﻿Imports System.Xml
+Imports System.Text
+Imports System
+Imports System.IO
+Imports System.Security
+Imports System.Security.Principal.WindowsIdentity
+Public Class Filechooser
+    Public strOutputFolder As String = ""
+    Public xmlelements As String() = {"posx", "posy", "width", "height", "textoffsetx", "textoffsety", "radiowidth", "radioheight", "radioposx", "radioposy", "textwidth", "border", "size", "bordersize", "itemgap"}
+    Public xmlattributes As String(,)
+    Public doc As New XmlDocument()
+    Public multiplyFactor As Double = 1.5
+    Public Filenames As String() = {}
+    Public SafeFilenames As String() = {}
+    Public elementlist As XmlNodeList
+    Public TempLetter As String
+    Public ElementCounter As String
+    Private Sub Filechooser_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ConversionDropDown.Items.Add("720p --> 1080p")
+        ConversionDropDown.Items.Add("No Change")
+        ConversionDropDown.SelectedIndex = 0
+        EncodingDropDown.Items.Add("UTF-8")
+        EncodingDropDown.Items.Add("ANSI")
+        EncodingDropDown.SelectedIndex = 0
+        OutputLog.AppendText("Program started" & vbCrLf)
+    End Sub
+
+    Private Sub ChooseButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ChooseButton.Click
+
+        OpenFileDialog.Title = "Choose XML file"
+        OpenFileDialog.Filter = "XML Files|*.xml"
+        Dim DidWork As Integer = OpenFileDialog.ShowDialog()
+        If DidWork = DialogResult.Cancel Then
+
+        Else
+            OutputButton.Visible = True
+            OutputLabel.Visible = True
+            Filenames = OpenFileDialog.FileNames
+            SafeFilenames = OpenFileDialog.SafeFileNames
+            If Filenames.Length = 1 Then
+                OutputLog.AppendText("XML File chosen: " + SafeFilenames(0) & vbCrLf)
+                xmlname.Text = SafeFilenames(0)
+            Else
+                OutputLog.AppendText("Amount Of Files Chosen: " + Filenames.Length.ToString & vbCrLf)
+                For i = 0 To Filenames.Length - 1
+                    OutputLog.AppendText(SafeFilenames(i) & vbCrLf)
+                Next i
+                xmlname.Text = Filenames.Length.ToString + " Files chosen"
+            End If
+            If strOutputFolder <> "" Then
+                ConvertButton.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Public Sub ConvertButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ConvertButton.Click
+        ' Create an XML declaration. 
+        Dim xmldecl As XmlDeclaration
+        xmldecl = doc.CreateXmlDeclaration("1.0", Nothing, Nothing)
+        Select Case EncodingDropDown.SelectedIndex
+            Case 0
+                xmldecl.Encoding = "UTF-8"
+            Case 1
+                xmldecl.Encoding = "ISO-8859-1"
+        End Select
+        xmldecl.Standalone = "yes"
+        Select Case ConversionDropDown.SelectedIndex
+            Case 0
+                multiplyFactor = 1.5
+                OutputLog.AppendText("converting 720 ==> 1080: Factor 1.5" & vbCrLf)
+            Case 1
+                multiplyFactor = 1
+                OutputLog.AppendText("only converting Encoding / adding Headers" & vbCrLf)
+        End Select
+        ElementCounter = 0
+        Dim errorcounter = 0
+        For j = 0 To Filenames.Length - 1
+            Try
+                OutputLog.AppendText("Processing " + SafeFilenames(j) & vbCrLf)
+                doc.Load(Filenames(j))
+                If HeaderOption.Checked Then
+                    Dim root As XmlElement = doc.DocumentElement
+                    doc.InsertBefore(xmldecl, root)
+                End If
+                If multiplyFactor <> 1 Then
+                    For i = 0 To xmlelements.Length - 1
+                        changeElements(xmlelements(i))
+                    Next i
+                    changeAttributes()
+                End If
+                Dim wrtr As XmlTextWriter = Nothing
+                Select Case EncodingDropDown.SelectedIndex
+                    Case 0
+                        wrtr = New XmlTextWriter(strOutputFolder + "\" + SafeFilenames(j), Encoding.UTF8)
+                    Case 1
+                        wrtr = New XmlTextWriter(strOutputFolder + "\" + SafeFilenames(j), Encoding.ASCII)
+                End Select
+                wrtr.Formatting = Formatting.Indented
+                doc.WriteTo(wrtr)
+                wrtr.Close()
+                OutputLog.AppendText(SafeFilenames(j) + " created successfully" & vbCrLf)
+                ElementCounter = ElementCounter + 1
+            Catch xmlex As XmlException                  ' Handle the Xml Exceptions here.
+                OutputLog.AppendText(xmlex.Message)
+                errorcounter = errorcounter + 1
+            Catch ex As Exception                        ' Handle the generic Exceptions here.
+                OutputLog.AppendText(ex.Message)
+                errorcounter = errorcounter + 1
+            End Try
+        Next j
+        OutputLog.AppendText("All Files converted" & vbCrLf)
+        MsgBox(ElementCounter + " XML Files converted." & vbCrLf & "Errors: " + errorcounter.ToString)
+        errorcounter = 0
+        ReDim Filenames(0)
+        ReDim SafeFilenames(0)
+        xmlname.Text = ""
+        ConvertButton.Enabled = False
+
+    End Sub
+
+    Private Sub OutputButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OutputButton.Click
+        Do
+            FolderBrowserDialog.Description = "Choose Output Folder"
+
+            Dim DidWork As Integer = FolderBrowserDialog.ShowDialog()
+            If DidWork = DialogResult.Cancel Then
+
+            Else
+                strOutputFolder = FolderBrowserDialog.SelectedPath
+                If Filenames(0) <> "" Then
+                    ConvertButton.Enabled = True
+                End If
+                OutputLabel.Text = strOutputFolder + "\"
+                OutputLog.AppendText("Output Folder chosen:" & vbCrLf)
+                OutputLog.AppendText(strOutputFolder & vbCrLf)
+            End If
+            If (strOutputFolder + "\" + SafeFilenames(0) = Filenames(0)) Then
+                MsgBox("You´ve chosen the soure directory. please change the output path.")
+            End If
+        Loop While (strOutputFolder + "\" + SafeFilenames(0) = Filenames(0))
+
+
+    End Sub
+
+    Sub changeElements(ByVal tag As String)
+        If DebugOutput.Checked Then
+            OutputLog.AppendText("Changing " + tag + " values..." & vbCrLf)
+        End If
+        elementlist = doc.GetElementsByTagName(tag)
+        For i = 0 To elementlist.Count - 1
+            If DebugOutput.Checked Then
+                OutputLog.AppendText(elementlist(i).InnerXml.ToString & " ==> " & ConvertValue(elementlist(i).InnerXml) & vbCrLf)
+            End If
+            elementlist(i).InnerXml = ConvertValue(elementlist(i).InnerXml)
+        Next i
+    End Sub
+
+    Function ConvertValue(ByVal InputString As String) As String
+        Dim number As Integer
+        ConvertValue = InputString
+        If Int32.TryParse(InputString, number) Then
+            ConvertValue = Math.Round(InputString * multiplyFactor)
+        Else
+            If DebugOutput.Checked Then
+                OutputLog.AppendText("Attempted conversion of " + InputString + " failed." & vbCrLf)
+            End If
+            If InputString.Length > 1 Then
+                TempLetter = InputString.ToString.Substring(InputString.Length - 1, 1)
+                InputString = InputString.ToString.Substring(0, InputString.Length - 1)
+                If DebugOutput.Checked Then
+                    OutputLog.AppendText("Removed last letter... " & vbCrLf)
+                    OutputLog.AppendText(InputString & vbCrLf)
+                End If
+                If Int32.TryParse(InputString, number) Then
+                    ConvertValue = Math.Round(number * multiplyFactor).ToString + TempLetter
+                    '   ConvertValue = Math.Round(number * multiplyFactor).ToString
+                    If DebugOutput.Checked Then
+                        OutputLog.AppendText("Conversion successful" & vbCrLf)
+                    End If
+                    OutputLog.AppendText(InputString & vbCrLf)
+                Else
+                    If InputString.Length > 1 Then
+                        TempLetter = InputString.ToString.Substring(InputString.Length - 1, 1)
+                        InputString = InputString.ToString.Substring(0, InputString.Length - 1)
+                        If DebugOutput.Checked Then
+                            OutputLog.AppendText("Removed last letter... " & vbCrLf)
+                            OutputLog.AppendText(InputString & vbCrLf)
+                        End If
+                        If Int32.TryParse(InputString, number) Then
+                            '      ConvertValue = Math.Round(number * multiplyFactor).ToString + TempLetter
+                            ConvertValue = Math.Round(number * multiplyFactor).ToString
+                            If DebugOutput.Checked Then
+                                OutputLog.AppendText("Conversion successful" & vbCrLf)
+                            End If
+                            OutputLog.AppendText(InputString & vbCrLf)
+                        Else
+                            If DebugOutput.Checked Then
+                                OutputLog.AppendText("not converted... " & vbCrLf)
+                            End If
+                        End If
+                    Else
+                        If DebugOutput.Checked Then
+                            OutputLog.AppendText("not converted... " & vbCrLf)
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Function
+    Function ConvertValue(ByVal InputString As String, ByVal orientation As String) As String
+        Dim number As Integer
+        ConvertValue = InputString
+        If Int32.TryParse(InputString, number) Then
+            ConvertValue = Math.Round(InputString * multiplyFactor)
+        Else
+            If DebugOutput.Checked Then
+                OutputLog.AppendText("Attempted conversion of " + InputString + " failed." & vbCrLf)
+            End If
+            If InputString.Length > 1 Then
+                TempLetter = InputString.ToString.Substring(InputString.Length - 1, 1)
+                InputString = InputString.ToString.Substring(0, InputString.Length - 1)
+                If DebugOutput.Checked Then
+                    OutputLog.AppendText("Removed last letter... " & vbCrLf)
+                    OutputLog.AppendText(InputString & vbCrLf)
+                End If
+                If Int32.TryParse(InputString, number) Then
+                    ConvertValue = Math.Round(number * multiplyFactor).ToString + TempLetter
+                    '   ConvertValue = Math.Round(number * multiplyFactor).ToString
+                    If DebugOutput.Checked Then
+                        OutputLog.AppendText("Conversion successful" & vbCrLf)
+                    End If
+                    OutputLog.AppendText(InputString & vbCrLf)
+                Else
+                    If InputString.Length > 1 Then
+                        TempLetter = InputString.ToString.Substring(InputString.Length - 1, 1)
+                        InputString = InputString.ToString.Substring(0, InputString.Length - 1)
+                        If DebugOutput.Checked Then
+                            OutputLog.AppendText("Removed last letter... " & vbCrLf)
+                            OutputLog.AppendText(InputString & vbCrLf)
+                        End If
+                        If Int32.TryParse(InputString, number) Then
+                            '      ConvertValue = Math.Round(number * multiplyFactor).ToString + TempLetter
+                            ConvertValue = Math.Round(number * multiplyFactor).ToString
+                            If DebugOutput.Checked Then
+                                OutputLog.AppendText("Conversion successful" & vbCrLf)
+                            End If
+                            OutputLog.AppendText(InputString & vbCrLf)
+                        Else
+                            If DebugOutput.Checked Then
+                                OutputLog.AppendText("not converted... " & vbCrLf)
+                            End If
+                        End If
+                    Else
+                        If DebugOutput.Checked Then
+                            OutputLog.AppendText("not converted... " & vbCrLf)
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Function
+
+    Sub convertString(ByRef CompleteString As String)
+        Dim NewString As String = ""
+        Dim TempString As String = CompleteString
+        Dim IndexStart = 0
+        For i = 0 To CompleteString.Length - 1
+            If CompleteString(i) = "," Then
+                If DebugOutput.Checked Then
+                    OutputLog.AppendText("comma detected. Pos i:" + i.ToString & vbCrLf)
+                End If
+                NewString = NewString + ConvertValue(CompleteString.Substring(IndexStart, i - IndexStart)) + ","
+                IndexStart = i + 1
+            End If
+        Next
+        CompleteString = NewString + ConvertValue((CompleteString.Substring(IndexStart, CompleteString.Length - IndexStart)))
+        If DebugOutput.Checked Then
+            OutputLog.AppendText(TempString & " ==> " & CompleteString & vbCrLf)
+        End If
+    End Sub
+
+    Sub convertStringifseveral(ByRef CompleteString As String)
+        Dim NewString As String = ""
+        Dim IndexStart = 0
+        Dim TempString As String = CompleteString
+        If CompleteString.Contains(",") Then
+            For i = 0 To CompleteString.Length - 1
+                If CompleteString(i) = "," Then
+                    If DebugOutput.Checked Then
+                        OutputLog.AppendText("comma detected. Pos i:" + i.ToString & vbCrLf)
+                    End If
+                    NewString = NewString + ConvertValue(CompleteString.Substring(IndexStart, i - IndexStart)) + ","
+                    IndexStart = i + 1
+                End If
+            Next
+            CompleteString = NewString + ConvertValue((CompleteString.Substring(IndexStart, CompleteString.Length - IndexStart)))
+            If DebugOutput.Checked Then
+                OutputLog.AppendText(TempString & " ==> " & CompleteString & vbCrLf)
+            End If
+        End If
+    End Sub
+    Sub convertStringifseveralstretch(ByRef CompleteString As String)
+        Dim NewString As String = ""
+        Dim IndexStart = 0
+        Dim TempString As String = CompleteString
+        Dim isX As Boolean = True
+        If CompleteString.Contains(",") Then
+            For i = 0 To CompleteString.Length - 1
+                If CompleteString(i) = "," Then
+                    If DebugOutput.Checked Then
+                        OutputLog.AppendText("comma detected. Pos i:" + i.ToString & vbCrLf)
+                    End If
+                    NewString = NewString + ConvertValue(CompleteString.Substring(IndexStart, i - IndexStart)) + ","
+                    If isX = True Then
+                        isX = False
+                    Else
+                        isX = True
+                    End If
+                    IndexStart = i + 1
+                End If
+            Next
+            CompleteString = NewString + ConvertValue((CompleteString.Substring(IndexStart, CompleteString.Length - IndexStart)))
+            If DebugOutput.Checked Then
+                OutputLog.AppendText(TempString & " ==> " & CompleteString & vbCrLf)
+            End If
+        End If
+    End Sub
+
+    Sub changeAttributes()
+        If DebugOutput.Checked Then
+            OutputLog.AppendText("Changing Attributes..." & vbCrLf)
+        End If
+        elementlist = doc.SelectNodes("//animation[@effect='zoom'] | //effect[@type='zoom']")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("start") Is Nothing Then
+                convertStringifseveral(elementlist(i).Attributes("start").InnerText)
+            End If
+            If Not elementlist(i).Attributes("end") Is Nothing Then
+                convertStringifseveral(elementlist(i).Attributes("end").InnerText)
+            End If
+            If Not elementlist(i).Attributes("zoom") Is Nothing Then
+                convertStringifseveral(elementlist(i).Attributes("zoom").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//animation[@effect='slide'] | //effect[@type='slide']")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("start") Is Nothing Then
+                convertString(elementlist(i).Attributes("start").InnerText)
+            End If
+            If Not elementlist(i).Attributes("end") Is Nothing Then
+                convertString(elementlist(i).Attributes("end").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//animation[@effect='rotatex'] | //animation[@effect='rotatey'] | //animation[@effect='rotate'] | //effect[@type='rotate'] | //effect[@type='rotatex'] | //effect[@type='rotatey'] | //animation[@effect='slide'] | //effect[@type='slide']")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("center") Is Nothing Then
+                convertString(elementlist(i).Attributes("center").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//texturefocus | //texture | //texturenofocus | //bordertexture | //texturesliderbackground | //texturesliderbar | //texturesliderbarfocus | //alttexturenofocus | //alttexturefocus | //midtexture")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("border") Is Nothing Then
+                convertString(elementlist(i).Attributes("border").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//focusedlayout | //itemlayout")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("width") Is Nothing Then
+                convertString(elementlist(i).Attributes("width").InnerText)
+            End If
+            If Not elementlist(i).Attributes("height") Is Nothing Then
+                convertString(elementlist(i).Attributes("height").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//hitrect | //camera")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("x") Is Nothing Then
+                convertString(elementlist(i).Attributes("x").InnerText)
+            End If
+            If Not elementlist(i).Attributes("y") Is Nothing Then
+                convertString(elementlist(i).Attributes("y").InnerText)
+            End If
+            If Not elementlist(i).Attributes("h") Is Nothing Then
+                convertString(elementlist(i).Attributes("h").InnerText)
+            End If
+            If Not elementlist(i).Attributes("w") Is Nothing Then
+                convertString(elementlist(i).Attributes("w").InnerText)
+            End If
+        Next
+        elementlist = doc.SelectNodes("//width | //height")
+        For i = 0 To elementlist.Count - 1
+            If Not elementlist(i).Attributes("min") Is Nothing Then
+                convertString(elementlist(i).Attributes("min").InnerText)
+            End If
+            If Not elementlist(i).Attributes("max") Is Nothing Then
+                convertString(elementlist(i).Attributes("max").InnerText)
+            End If
+        Next
+    End Sub
+
+End Class
