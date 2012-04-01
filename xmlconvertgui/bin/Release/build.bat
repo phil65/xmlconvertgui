4c533a4a2184a2263dceee462486a686e38700a9
phil65 <philipptemminghoff@googlemail.com>
2012-04-01 04:23:40 +0200

    forgot to uncomment

--- xmlconvertgui/bin/Release/build.bat
+++ xmlconvertgui/bin/Release/build.bat
@@ -7,19 +7,19 @@
  if exist %3 rmdir %3 /S /Q
  md %3\media\
 
- REM ECHO ----------------------------------------
- REM Echo Building main skin XBT
- REM ECHO ----------------------------------------
- REM START /B /WAIT %2 -dupecheck -input %1\media -output %3\media\Textures.xbt
+ ECHO ----------------------------------------
+ Echo Building main skin XBT
+ ECHO ----------------------------------------
+ START /B /WAIT %2 -dupecheck -input %1\media -output %3\media\Textures.xbt
 
- REM ECHO ----------------------------------------
- REM Echo Finished building main skin XBT
- REM if exist %1\themes (
-     REM Echo Building theme skin XBT Files
-     REM ECHO ----------------------------------------
-     REM for /f "tokens=*" %%f in ('dir /b/ad %1\themes') do START /B /WAIT %2 -dupecheck -input %1\themes\%%f -output %3\media\%%f.xbt
-     REM Echo Finished Building theme skin XBT Files
- REM )
+ ECHO ----------------------------------------
+ Echo Finished building main skin XBT
+ if exist %1\themes (
+     Echo Building theme skin XBT Files
+     ECHO ----------------------------------------
+     for /f "tokens=*" %%f in ('dir /b/ad %1\themes') do START /B /WAIT %2 -dupecheck -input %1\themes\%%f -output %3\media\%%f.xbt
+     Echo Finished Building theme skin XBT Files
+ )
  ECHO ----------------------------------------
  Echo Copying other files
  ECHO ----------------------------------------
