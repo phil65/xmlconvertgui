312c50c311ce9082256604d0002a6a5fdddc238c
bumpaneer <bumpaneer@gmail.com>
2014-05-25 22:03:21 -0400

    Add FolderSelect Dialog
    Nicer dialog for selecting the skin folder

---
+++ FolderSelectDialog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FolderSelect
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Environment.OSVersion.Platform.ToString());
+			Console.WriteLine(Environment.OSVersion.Version.Major);
+
+			{
+				var fsd = new FolderSelectDialog();
+				fsd.Title = "What to select";
+				fsd.InitialDirectory = @"c:\";
+				if (fsd.ShowDialog(IntPtr.Zero))
+				{
+					Console.WriteLine(fsd.FileName);
+				}
+			}
+		}
+	}
+}
